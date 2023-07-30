12c79b8038e132379f4e91ba4c007e0e52568e8e
bogdaba <bogdaba@github.com>
2023-07-30 10:53:51 +0200

    nyo

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,7 +1,14 @@
 # This is your home-manager configuration file
 # Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
 
-{ inputs, lib, config, pkgs, ... }: {
+{ inputs, lib, config, pkgs, ... }: 
+
+let
+  unstable = import <nixpkgs-unstable> {
+    config = config.nixpkgs.config;
+  };
+in
+{
   # You can import other home-manager modules here
   imports = [
     # If you want to use home-manager modules from other flakes (such as nix-colors):
