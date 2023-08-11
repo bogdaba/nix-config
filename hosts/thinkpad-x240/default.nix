0c5907245441fcd76d25c6f1ec7bd153b20bd915
bogdaba <bogdaba@github.com>
2023-08-12 00:13:34 +0200

    nyp=o

--- hosts/thinkpad-x240/default.nix
+++ hosts/thinkpad-x240/default.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   imports =
@@ -10,6 +10,72 @@
       ../home.nix
       ./hardware-configuration.nix
     ];
+
+
+
+  #
+# Modules
+modules = {
+  desktop = {
+    bspwm.enable = true;
+    apps = {
+      rofi.enable = true;
+      # godot.enable = true;
+    };
+    browsers = {
+      default = "firefox";
+      brave.enable = true;
+      firefox.enable = true;
+      qutebrowser.enable = true;
+    };
+    gaming = {
+      steam.enable = true;
+      # emulators.enable = true;
+      # emulators.psx.enable = true;
+    };
+    media = {
+      daw.enable = true;
+      documents.enable = true;
+      graphics.enable = true;
+      mpv.enable = true;
+      recording.enable = true;
+      spotify.enable = true;
+    };
+    term = {
+      default = "xst";
+      st.enable = true;
+    };
+    vm = {
+      qemu.enable = true;
+    };
+  };
+  dev = {
+    node.enable = true;
+    rust.enable = true;
+  #  python.enable = true;
+  };
+  editors = {
+    #default = "nvim";
+    #emacs.enable = true;
+    #vim.enable = true;
+  };
+  shell = {
+    #adl.enable = true;
+    #vaultwarden.enable = true;
+    direnv.enable = true;
+    git.enable    = true;
+    #gnupg.enable  = true;
+    #tmux.enable   = true;
+    zsh.enable    = true;
+  };
+  services = {
+    #ssh.enable = true;
+    #docker.enable = true;
+    # Needed occasionally to help the parental units with PC problems
+    # teamviewer.enable = true;
+  };
+  #theme.active = "alucard";
+};
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
@@ -150,7 +216,7 @@
 #    ./hardware-configuration.nix
 #  ];
 #
-#  ## Modules
+#  # Modules
 #  modules = {
 #    desktop = {
 #      bspwm.enable = true;
