2a7997ed6fa98146c757715ae7c9ff25882029ed
bogdaba <bogdaba@github.com>
2023-08-12 00:09:37 +0200

    nyo

--- modules/services/docker.nix
+++ modules/services/docker.nix
@@ -15,8 +15,8 @@ in {
       docker-compose
     ];
 
-    env.DOCKER_CONFIG = "$XDG_CONFIG_HOME/docker";
-    env.MACHINE_STORAGE_PATH = "$XDG_DATA_HOME/docker/machine";
+    #env.DOCKER_CONFIG = "$XDG_CONFIG_HOME/docker";
+    #env.MACHINE_STORAGE_PATH = "$XDG_DATA_HOME/docker/machine";
 
     user.extraGroups = [ "docker" ];
 
