4aa8618236dd6b74160a547e99a15b51d27c442c
bogdaba <bogdaba@github.com>
2023-08-12 01:10:36 +0200

    d

--- flake.nix
+++ flake.nix
@@ -58,7 +58,6 @@
       # NixOS configuration entrypoint
       # Available through 'nixos-rebuild --flake .#your-hostname'
       nixosConfigurations = {
-        # FIXME replace with your hostname
         thinkpad-x240 = nixpkgs.lib.nixosSystem {
           specialArgs = { inherit inputs outputs; };
           modules = [
@@ -70,16 +69,16 @@
 
       # Standalone home-manager configuration entrypoint
       # Available through 'home-manager --flake .#your-username@your-hostname'
-      homeConfigurations = {
+      #homeConfigurations = {
         # FIXME replace with your username@hostname
-        "your-username@your-hostname" = home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-          extraSpecialArgs = { inherit inputs outputs; };
-          modules = [
+        #"your-username@your-hostname" = home-manager.lib.homeManagerConfiguration {
+          #pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+          #extraSpecialArgs = { inherit inputs outputs; };
+          #modules = [
             # > Our main home-manager configuration file <
-            ./home-manager/home.nix
-          ];
-        };
-      };
+            #./home-manager/home.nix
+          #];
+        #};
+      #};
     };
 }
